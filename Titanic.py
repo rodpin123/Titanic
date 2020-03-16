5651a7ff230e4bf6d2782ac09588878b76869053
Rodrigo Pinheiro <Rodrigo.Pinheiro@technipfmc.com>
2020-03-16 19:09:53 -0300

    Applied Random Forest Classifier

--- Titanic.py
+++ Titanic.py
@@ -2,7 +2,8 @@ import numpy as np
 import pandas as pd
 import os
 
-import os
+from sklearn.ensemble import RandomForestClassifier
+
 #for dirname, _, filenames in os.walk('.'):
 #    for filename in filenames:
 #        print(os.path.join(dirname, filename))
@@ -24,3 +25,19 @@ print("% of men who survived:", rate_men,"%")
 survivors = train_data["Survived"]
 rate_survivors = round(sum(survivors)/len(survivors), 2)
 print("% of survivors in total:", rate_survivors, "%")
+
+
+
+y = train_data["Survived"]
+
+features = ["Pclass", "Sex", "SibSp", "Parch"]
+X = pd.get_dummies(train_data[features])
+X_test = pd.get_dummies(test_data[features])
+
+model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
+model.fit(X, y)
+predictions = model.predict(X_test)
+
+output = pd.DataFrame({'PassengerId': test_data.PassengerId, 'Survived': predictions})
+output.to_csv('C:\my_submission.csv', index=False)
+print("Your submission was successfully saved!")
