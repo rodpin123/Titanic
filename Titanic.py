5ba154c7283dd7e8f69dadef071a8f4aa3e1bf9b
Rodrigo Pinheiro <Rodrigo.Pinheiro@technipfmc.com>
2020-03-16 14:40:22 -0300

    Rounded values and commented not so useful info

--- Titanic.py
+++ Titanic.py
@@ -3,22 +3,24 @@ import pandas as pd
 import os
 
 import os
-for dirname, _, filenames in os.walk('.'):
-    for filename in filenames:
-        print(os.path.join(dirname, filename))
+#for dirname, _, filenames in os.walk('.'):
+#    for filename in filenames:
+#        print(os.path.join(dirname, filename))
 
 train_data = pd.read_csv("./input/train.csv")
-print(train_data.head())
+#print(train_data.head())
 
 test_data = pd.read_csv("./input/test.csv")
-print(test_data.head())
+#print(test_data.head())
 
 women = train_data.loc[train_data.Sex == 'female']["Survived"]
-rate_women = sum(women)/len(women)
-
-print("% of women who survived:", rate_women)
+rate_women = round(sum(women)/len(women),2)
+print("% of women who survived:", rate_women,"%")
 
 men = train_data.loc[train_data.Sex == 'male']["Survived"]
-rate_men = sum(men)/len(men)
+rate_men = round(sum(men)/len(men),2)
+print("% of men who survived:", rate_men,"%")
 
-print("% of men who survived:", rate_men)
+survivors = train_data["Survived"]
+rate_survivors = round(sum(survivors)/len(survivors), 2)
+print("% of survivors in total:", rate_survivors, "%")
